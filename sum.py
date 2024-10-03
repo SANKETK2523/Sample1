017a37f1161f7387c41ff6450e4a19bcc8d57d31
SK2003 <161557906+SANKETK2523@users.noreply.github.com>
2024-10-03 13:10:35 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,12 +1,10 @@
-# Sum of natural numbers up to num
 
-num = 16
+num = 10
 
 if num < 0:
    print("Enter a positive number")
 else:
    sum = 0
-   # use while loop to iterate until zero
    while(num > 0):
        sum += num
        num -= 1
